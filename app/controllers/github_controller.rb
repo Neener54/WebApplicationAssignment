ce7ac61cacabedbec505d6a6699389e789ccf029
ankit <ankit.chachada@gmail.com>
2016-04-30 22:57:23 -0700

    changes in github UI

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -3,9 +3,18 @@ require 'json'
  
 class GithubController < ApplicationController
   def search
-    url = 'https://api.github.com/users/ankitchachada'
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
+    response = get_data("https://api.github.com/users/#{params[:search]}")
     @data = JSON.parse(response)
+    if @data["message"].nil? 
+      @repos = JSON.parse(get_data(@data["repos_url"]))
+    end
+  end
+
+  def get_data(url)
+  	begin
+  	  Net::HTTP.get(URI(url))
+    rescue Exception => e
+  	 puts e
+    end
   end
 end
